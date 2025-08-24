0b06d16cbf0c10a4a40948cdfe7cf7968cfe388b
maduvitorino <maria.vitorino@gran.com>
2025-08-24 11:55:53 -0300

    init: inicia projeto

---
+++ src/main/java/com/unipass/unipass_manager_api/UnipassManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.unipass.unipass_manager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnipassManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnipassManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unipass/unipass_manager_api/UnipassManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unipass.unipass_manager_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnipassManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
