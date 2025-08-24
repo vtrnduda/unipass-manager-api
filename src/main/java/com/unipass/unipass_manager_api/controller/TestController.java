a77f937ec5550f2b9d3c349b07d24c26ebcf1772
vtrnduda <vtrnduda@gmail.com>
2025-08-24 12:31:47 -0300

    feat: adiciona controller com um endpoint para teste

--- src/main/java/com/unipass/unipass_manager_api/controller/TestController.java
+++ src/main/java/com/unipass/unipass_manager_api/controller/TestController.java
@@ -1,4 +1,20 @@
 package com.unipass.unipass_manager_api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "API funcionando!";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "Sistema online";
+    }
 }
