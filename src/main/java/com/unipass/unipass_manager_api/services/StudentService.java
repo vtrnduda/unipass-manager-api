6b55a7e36eedab8bc5b289f1c2d3cae31850299d
janderson01 <jandersonsantos030@gmail.com>
2025-08-25 17:14:51 -0300

    feat: adiciona os métodos em StudentServices

--- src/main/java/com/unipass/unipass_manager_api/services/StudentService.java
+++ src/main/java/com/unipass/unipass_manager_api/services/StudentService.java
@@ -1,6 +1,9 @@
 package com.unipass.unipass_manager_api.services;
 
 import com.unipass.unipass_manager_api.Repository.StudentRepository;
+import com.unipass.unipass_manager_api.model.Student;
+
+import java.util.List;
 
 public class StudentService {
     private final StudentRepository studentRepository;
@@ -9,4 +12,21 @@ public class StudentService {
         this.studentRepository = studentRepository;
     }
 
+    public List<Student> findAll() {
+        return this.studentRepository.findAll();
+    }
+
+    public Student findById(Long id) {
+        return this.studentRepository.findById(id).orElse(null);
+    }
+
+    public boolean create(Student student) {
+        return this.studentRepository.save(student) != null?true:false;
+
+    }
+
+    public boolean delete(Student student) {
+        this.studentRepository.delete(student);
+        return true;
+    }
 }
